4d265dfff850b55ff9f0254db50743cc8ff42c54
james <james.milne@protonmail.com>
2025-04-30 02:20:22 +0000

    Added tests, and added test to rockspec

--- test.lua
+++ test.lua
@@ -18,6 +18,34 @@ assert(dedlit.pushsyntax)
 assert(dedlit.issyntax)
 assert(dedlit.syntax)
 
+assert(dedlit.syntax() == "```(.-)```")
+assert(dedlit.issyntax() == false)
+assert(dedlit.issyntax(dedlit.syntax()) == true)
+
+assert(dedlit.pushsyntax("##(.-)##"))
+assert(dedlit.issyntax() == false)
+assert(dedlit.issyntax("```(.-)```") == false)
+assert(dedlit.issyntax("##(.-)##") == true)
+assert(dedlit.popsyntax())
+assert(dedlit.issyntax("##(.-)##") == false)
+assert(dedlit.issyntax("```(.-)```") == true)
+
+do
+	local test_limit = 1000
+	for i=1, test_limit do
+		local v = string.format("#%d(.-)%d#", i, i)
+		assert(dedlit.pushsyntax(v))
+		assert(dedlit.issyntax(v))
+	end
+
+	for i=test_limit, 1, -1 do
+		local v = string.format("#%d(.-)%d#", i, i)
+		assert(dedlit.issyntax(v))
+		assert(dedlit.popsyntax())
+	end
+end
+
 assert(dedlit.parse)
 assert(dedlit.eval)
 assert(dedlit.litfile)
+
